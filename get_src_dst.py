6fef745abf38153f1db52a1d5f9d88ffa2c7b817
Shreyansh Goyal <94932358+ShreyanshGoyal17@users.noreply.github.com>
2023-03-28 01:06:15 -0400

    Add files via upload

---
+++ get_src_dst.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def create_src_dst(img_src_to_dst):
+    src_pts = []#img_1to2[1][1]
+    dst_pts = []
+    for i in range(len(img_src_to_dst)):
+        a = img_src_to_dst[i][1]
+        b = img_src_to_dst[i][2]
+        src_pts.append(a)
+        dst_pts.append(b)
+    src_pts = np.asarray(src_pts)
+    dst_pts = np.asarray(dst_pts)
+    return src_pts, dst_pts
